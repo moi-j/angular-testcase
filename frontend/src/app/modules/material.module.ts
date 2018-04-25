dd440de137438fa19921e79c17cd53fda47214c4
Moisés <jimenezmartinmoises@gmail.com>
2018-04-25 13:16:58 +0200

    setting initial configuration
    Setting initial configuration

--- frontend/src/app/modules/material.module.ts
+++ frontend/src/app/modules/material.module.ts
@@ -1,15 +1,24 @@
 import {NgModule} from "@angular/core";
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
+    BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
