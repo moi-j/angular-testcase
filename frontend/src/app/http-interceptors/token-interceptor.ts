8942835be80730b4c0538fed70be55350fda34c7
Moisés <jimenezmartinmoises@gmail.com>
2018-04-28 22:11:00 +0200

    Updating token interceptor

--- frontend/src/app/http-interceptors/token-interceptor.ts
+++ frontend/src/app/http-interceptors/token-interceptor.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
 import {
   HttpInterceptor,
   HttpRequest,
-  HttpResponse,
-  HttpErrorResponse,
   HttpHandler,
-  HttpEvent
 } from '@angular/common/http';
 
 import {AdminService} from "../services/admin.service";
@@ -18,6 +14,7 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor(private _admin: AdminService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+
     // Get the auth token from the service.
     const authToken = this._admin.getToken();
 
@@ -26,17 +23,20 @@ export class TokenInterceptor implements HttpInterceptor {
     const authReq = req.clone({
       setHeaders: { Authorization: 'Token ' + authToken }
     });
-    console.log(authReq, req);
+
     // send cloned request with header to the next handler.
-    return next.handle(authReq)
-      .pipe(tap((event: HttpEvent<any>) => {
-          if (event){
-            console.log('Token interceptor event', event);
-          };
-      }, err => {
-        if (err instanceof HttpErrorResponse) {
-          console.log('token interceptor error', err);
-        }
-      }));
+    console.log(authReq);
+    return next.handle(authReq);
+
+                                // CATCHING ERROR IN SUBSCRIBE INSTEAD
+      // .pipe(tap((event: HttpEvent<any>) => {
+      //     if (event){
+      //       console.log('Token interceptor event', event);
+      //     };
+      // }, err => {
+      //   if (err instanceof HttpErrorResponse) {
+      //     console.log('token interceptor error', err);
+      //   }
+      // }));
   }
 }
