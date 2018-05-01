13c5c6bb8ba2a34e0ef919d1b8fb78bc4062a23d
Moisés <jimenezmartinmoises@gmail.com>
2018-05-01 21:59:18 +0200

    styling

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UsersListComponent} from "../users-list/users-list.component";
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild(UsersListComponent) userList;
 
   constructor() { }
 
