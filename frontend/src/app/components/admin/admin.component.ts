385077aac88afdf8a91038124b1294cf94830d20
Moisés <jimenezmartinmoises@gmail.com>
2018-05-01 13:00:32 +0200

    Added viewChild for calling child methods
    Added viewChild for calling child methods and added unsubscriptions on Destroy Hook

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,27 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from "@angular/material";
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+
+import {MatDialog, MatSnackBar} from "@angular/material";
+
 import { User } from "../../interfaces/user";
+
 import { UserFormComponent } from "./user-form/user-form.component";
+import { UsersListComponent } from "../users-list/users-list.component";
+import {Subject} from "rxjs/Subject";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
+
+  @ViewChild(UsersListComponent) userList;
+
+  private destroy$ = new Subject<void>();
 
   constructor(
     public dialog: MatDialog,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
   }
 
+
   openDialog( user: User = null ): void {
     let dialogConfig = {
       data: user
-    }
-    let dialogRef = this.dialog.open( UserFormComponent, dialogConfig )
+    };
+    this.dialog.open( UserFormComponent, dialogConfig ).afterClosed().pipe(takeUntil(this.destroy$)).subscribe( user => {
+      (user) ? this.userList.refresh() : false;
+    }, err => {
+      this.snackBar.open(
+        'There was an error refreshing the list. Try to reload this page.',
+        'Ok');
+    });
   }
 
+  ngOnDestroy(){
+    this.dialog.closeAll();
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
