4f3a3c0aedb1cd44ccfdb0d1a6e019c3d75947d9
Moisés <jimenezmartinmoises@gmail.com>
2018-05-01 13:03:14 +0200

    Added form methods
    Added OnSubmit, Error Handler and unsubcriptions on Destroy Hook

--- frontend/src/app/components/admin/user-form/user-form.component.ts
+++ frontend/src/app/components/admin/user-form/user-form.component.ts
@@ -1,44 +1,65 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {User} from "../../../interfaces/user";
+import {Component, Inject, OnInit, OnDestroy} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { User } from "../../../interfaces/user";
 import { ibanValidator } from './validators/iban.directive';
+import { lettersValidator } from "./validators/letters.directive";
+import { UserService } from "../../../services/user.service";
+import {Subject} from "rxjs/Subject";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent implements OnInit, OnDestroy {
 
+  private destroy$ = new Subject<void>();
   public userForm: FormGroup;
+  public hasServerErrors: boolean = false;
+  public serverErrors: string[];
+  public serverErrorTypes: string[];
+  public isLoading: boolean = false;
+  public isUpdating: boolean = false;
 
   constructor(
+    private _user: UserService,
+    private fb: FormBuilder,
     public dialogRef: MatDialogRef<UserFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public user: User
+    @Inject(MAT_DIALOG_DATA) public user: User,
   ) {
-    if(!user){
-      this.user = {
-        first_name: '',
-        last_name: '',
-        iban: ''
-      }
+    this.createForm();
+    if(user){
+      this.isUpdating = true;
+      this.userForm.setValue({
+        'first_name': this.user.first_name,
+        'last_name': this.user.last_name,
+        'iban': this.user.iban
+      });
     }
   }
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  createForm() {
     this.userForm = new FormGroup({
-      'first_name': new FormControl(this.user.first_name, [
+      'first_name': new FormControl('', [
         Validators.required,
-        Validators.minLength(3),
+        lettersValidator(),
+        Validators.minLength(2),
       ]),
-      'last_name': new FormControl(this.user.last_name, [
+      'last_name': new FormControl('', [
         Validators.required,
-        Validators.minLength(3),
+        lettersValidator(),
+        Validators.minLength(2),
       ]),
-      'iban': new FormControl(this.user.iban, [
+      'iban': new FormControl('', [
         Validators.required,
-        ibanValidator()
+        Validators.minLength(15),
+        Validators.maxLength(32),
+        ibanValidator(),
+        // ibanLengthValidator(),
       ])
     });
   }
@@ -54,4 +75,35 @@ export class UserFormComponent implements OnInit {
   get iban() {
     return this.userForm.get('iban');
   }
+
+  onSubmit() {
+    if (this.userForm.valid){
+      this.isLoading = true;
+      this.user = this.userForm.value;
+      this._user.createUser(this.user).pipe(takeUntil(this.destroy$)).subscribe( newUser => {
+        this.isLoading = false;
+        this.dialogRef.close(newUser);
+      }, err => {
+        this.isLoading = false;
+        (err.error) ? this.errorHandler(err.error) : false;
+      });
+    } else {
+      console.log('invalid');
+    }
+  }
+
+  errorHandler(error){
+    this.serverErrorTypes = [];
+    this.serverErrors = error;
+    for(let ind in error){
+      this.serverErrorTypes.push(ind);
+    }
+    this.hasServerErrors = true;
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
